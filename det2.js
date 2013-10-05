e08ae7a2d26d4b8c57e76b828b20d2a8169200d2
Mikola Lysenko <mikolalysenko@gmail.com>
2013-10-05 11:00:31 -0500

    improved test case

--- det2.js
+++ det2.js
@@ -5,8 +5,6 @@ module.exports = robustDeterminant2
 
 function robustDeterminant2(m) {
 	var p = twoProduct(m[0][0], m[1][1])
-	var q = twoProduct(m[1][0], m[0][1])
-	q[0] *= -1
-	q[1] *= -1
-	return robustSum(p, q)
+	var q = twoProduct(-m[1][0], m[0][1])
+  return robustSum(p, q)
 }
--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
 "use strict"
 
+var glm = require("gl-matrix")
 var det2 = require("../det2.js")
 
 require("tape")(function(t) {
 
 	t.same(det2([[1, 0], [0, 1]]), [1])
+  
+  for(var i=0; i<100; ++i) {
+    var r = [ [ (10*Math.random())|0, (10*Math.random())|0 ], [ (10*Math.random())|0, (10*Math.random())|0 ] ]
+    t.same(det2(r), [glm.mat2.determinant(r[0].concat(r[1]))])
+  }
 	
 	t.end()
 })
